dc0892d8cfbd98d084ca870b04b121c0257b14e6
Jasmine Dang <ngandth.1992@gmail.com>
2020-11-02 20:47:42 +0800

    use context in class component"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React, {useContext, useState} from 'react';
 
+const ThemeContext = React.createContext();
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div className='App'>
+    <ThemeContext.Provider value={darkTheme}>
       <button>Click here</button>
-      <FuncChildren />
+      <FuncChild />
+      <ClassChild />
+    </ThemeContext.Provider>
+  );
+}
+
+function FuncChild() {
+  function themeStyles(darkTheme) {
+    return {
+      display: 'flex',
+      alignIems: 'center',
+      justifyContent: 'center',
+      backgroundColor: darkTheme ? 'black' : 'grey',
+      margin: '10px',
+      color: 'white',
+    };
+  }
+
+  return (
+    <div style={themeStyles(true)}>
+      <p>Function</p>
     </div>
   );
 }
 
-function FuncChildren() {
-  function themeStyles(darkTheme) {
+class ClassChild extends React.Component {
+  themeStyles(darkTheme) {
     return {
+      display: 'flex',
+      alignIems: 'center',
+      justifyContent: 'center',
       backgroundColor: darkTheme ? 'black' : 'grey',
-      height: '100px',
-      width: '100px',
+      color: 'white',
+      margin: '10px',
+      textAlign: 'center',
     };
   }
 
-  return <div style={themeStyles(true)}></div>;
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(darkTheme) => {
+          return (
+            <div style={this.themeStyles(darkTheme)}>
+              <p>Class</p>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
 
 export default App;
